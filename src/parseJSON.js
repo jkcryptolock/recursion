6153aa3b1d3b35adfaa81a31b507bcf0c3fbb1e8
John Connolly <jkconno@gmail.com>
2019-06-28 18:59:28 -0500

    parseJSON for objects is close to working, but not quite

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -30,10 +30,41 @@ var parseJSON = function(json) {
   }
 
   if ((json[0] === '[') && (json[json.length - 1] === ']')) {
+  	if (json === '[]'){
+  		return [];
+  	}
   	let contents = json.slice(1, -1);
   	let elements = contents.split(',');
   	let result = elements.map((el) => parseJSON(el));
   	return result;
   }
 
+  if (json[0] === '{' && json[json.length - 1] === '}') {
+  	var obj = {};
+  	if (json === '{}') {
+  		return {};
+  	}
+  	let objContents = json.slice(1, -1);
+  	//console.log(objContents)
+  	let colon = json.indexOf(':');
+  	//console.log(colon)
+  	let comma = json.indexOf(',');
+  	//console.log(comma)
+  	
+  	let key = parseJSON(json.slice(1, colon));
+  	
+  	if (comma !== -1){
+  		let value = parseJSON(json.slice(colon + 1, comma));
+  		obj[key] = value;
+  		json = json.slice(colon, comma)
+  		//console.log(value)
+  	} else {
+  		let value = parseJSON(json.slice(colon + 1));
+  		obj[key] = value;
+  		//console.log(value);
+  	}
+  	console.log(key)
+
+  	return obj;
+  }
 };
